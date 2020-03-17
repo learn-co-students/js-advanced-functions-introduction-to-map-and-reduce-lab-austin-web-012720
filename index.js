ea0cb3a9736c9a29166ea114a504b5079a0fda82
GoTo15 <github email address>
2020-03-17 14:30:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+function mapToNegativize(array) {
+  let retArray = [];
+  array.forEach(element => {
+    retArray.push(-1 * element);
+  });
+  return retArray;
+}
+
+function mapToNoChange(array) {
+  return array;
+}
+
+function mapToDouble(array) {
+  let retArray = [];
+  array.forEach(element => {
+    retArray.push(2 * element);
+  });
+  return retArray;
+}
+
+function mapToSquare(array) {
+  let retArray = [];
+  array.forEach(element => {
+    retArray.push(element * element);
+  });
+  return retArray;
+}
+
+function reduceToTotal(array, start = 0) {
+  let sum = start;
+  array.forEach(element => {
+    sum += element;
+  });
+  return sum;
+}
+
+function reduceToAllTrue(array) {
+  let test = true;
+  array.forEach(element => {
+    if (!element) {
+      test = false;
+    }
+  });
+  return test;
+}
+
+function reduceToAnyTrue(array) {
+  let test = false;
+  array.forEach(element => {
+    if (element) {
+      test = true;
+    }
+  });
+  return test;
+}
